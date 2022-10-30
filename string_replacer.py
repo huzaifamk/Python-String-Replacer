9666461f303a51cfb3a8449648bab9425378727b
HuzaifaMk <76698805+HuzaifaMk@users.noreply.github.com>
2022-10-30 20:27:40 +0500

    Updated Comments

--- string_replacer.py
+++ string_replacer.py
@@ -1,26 +1,26 @@
 def replace_strings():
-    # Open the file sales.txt.
+    # Open the file in read mode
     infile = open('./files/test.txt', 'r')
 
-    # Read the file's contents.
+    # Read the file's contents to a variable
     contents = infile.read()
 
-    # Print the file's contents.
+    # Print the file's contents
     print(contents)
 
-    # Close the file.
+    # Close the file
     infile.close()
 
-    # Replace each dollar sign with a pound sign.
+    # Replace each dollar sign with a pound sign
     contents = contents.replace('$', '£')
 
-    # Open the file sales.txt.
+    # Open the file sales.txt in write mode
     outfile = open('./files/new_file.txt', 'w')
 
-    # Write the modified contents to the file.
+    # Write the modified contents to the file
     outfile.write(contents)
 
-    # Close the file.
+    # Close the file
     outfile.close()
 
 replace_strings()
