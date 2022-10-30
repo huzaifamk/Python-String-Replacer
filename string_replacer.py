bf0457d6ecc8e9a6b5013a45cf74504b2c03c493
HuzaifaMk <76698805+HuzaifaMk@users.noreply.github.com>
2022-10-30 20:41:47 +0500

    Completed building modules and testing for multiple files

--- string_replacer.py
+++ string_replacer.py
@@ -1,5 +1,11 @@
+from genericpath import isfile
+from msilib.schema import Directory
+from ntpath import join
+from os import listdir
+
+
 def replace_strings():
-    # Open the file in read mode
+    # Open a file in read mode
     infile = open('./files/test.txt', 'r')
 
     # Read the file's contents to a variable
@@ -23,4 +29,36 @@ def replace_strings():
     # Close the file
     outfile.close()
 
+
+def replace_string_in_multiple_files():
+
+    files = ['file_one.txt', 'file_two.txt', 'file_three.txt']
+
+    # Read file names from a directory
+    Directory = './files'
+    files = [f for f in listdir(Directory) if isfile(join(Directory, f))]
+    print(files)
+
+    for file in files:
+
+        infile = open('./many_files/' + file, 'r')
+
+        contents = infile.read()
+
+        print(contents)
+
+        infile.close()
+
+        contents = contents.replace('Python', 'C++')
+
+        i = '1'
+
+        outfile = open('./many_files/' + file+'_'+i, 'w')
+
+        outfile.write(contents)
+
+        outfile.close()
+
+
 replace_strings()
+replace_string_in_multiple_files()
