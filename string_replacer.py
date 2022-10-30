894e47eec3b01e7f3524280b15f68b259c0632b5
HuzaifaMk <76698805+HuzaifaMk@users.noreply.github.com>
2022-10-30 20:18:01 +0500

    Adding logic

---
+++ string_replacer.py
@@ -0,0 +1,26 @@
+def replace_strings():
+    # Open the file sales.txt.
+    infile = open('sales.txt', 'r')
+
+    # Read the file's contents.
+    contents = infile.read()
+
+    # Print the file's contents.
+    print(contents)
+
+    # Close the file.
+    infile.close()
+
+    # Replace each dollar sign with a pound sign.
+    contents = contents.replace('$', '£')
+
+    # Open the file sales.txt.
+    outfile = open('sales.txt', 'w')
+
+    # Write the modified contents to the file.
+    outfile.write(contents)
+
+    # Close the file.
+    outfile.close()
+
+replace_strings()
